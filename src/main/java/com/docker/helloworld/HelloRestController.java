2a6bb1084c075a1a6dc1a8a61381fde3b8ff2555
Ankit Patel <anpatel@bestpeers.in>
2025-01-22 12:05:03 +0530

    Added Swagger-Ui

--- src/main/java/com/docker/helloworld/HelloRestController.java
+++ src/main/java/com/docker/helloworld/HelloRestController.java
@@ -1,14 +1,23 @@
 package com.docker.helloworld;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String sayHello() {
     	int a =10;
         return "Hello Welcome to Rest Controller!";
     }
+    @PostMapping("/user")
+    public ResponseEntity<String> createUser() {
+        System.out.println("Post API");
+        return ResponseEntity.ok("POST API Worked");
+    }
+
+
 }
