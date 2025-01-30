943928f76c46a813c969384cb162a0539de38345
Ankit Patel <anpatel@bestpeers.in>
2025-01-30 11:38:59 +0530

    Consumed Third-party Rest API and inserted data into MongoDB

--- src/main/java/com/mongodb/restApi/controllers/BookController.java
+++ src/main/java/com/mongodb/restApi/controllers/BookController.java
@@ -3,10 +3,14 @@ package com.mongodb.restApi.controllers;
 
 import com.mongodb.restApi.entity.Book;
 import com.mongodb.restApi.services.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +22,23 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    // Get Data From RestAPI By ID
+
+    @Operation(
+            summary = "Fetch Data From Book API and Insert into DB",
+            description = "TO fetch a Book by their unique ID from the RestAPI and Insert it Into Local Database."
+    )
+    @GetMapping("/api/{id}")
+    public String insertBooksData(@PathVariable String id) {
+
+        return bookService.insertBookData(id);
+    }
+
     // Create or update Book
+    @Operation(
+            summary = "Insert manual book data into Local DB",
+            description = "TO Insert Manual book data by sending data of Book object in request body"
+    )
     @PostMapping
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
         Book savedBook = bookService.saveBook(book);
@@ -26,12 +46,20 @@ public class BookController {
     }
 
     // Get all Books
+    @Operation(
+            summary = "To get All book data from mongoDB ",
+            description = "TO fetch all book data inserted in MongoDB"
+    )
     @GetMapping
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
     // Get Book by ID
+    @Operation(
+            summary = "Get single book data by Id from mongoDB ",
+            description = "TO fetch an book data inserted in MongoDB By Id"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable String id) {
         Optional<Book> book = bookService.getBookById(id);
@@ -42,8 +70,11 @@ public class BookController {
         }
     }
 
-
     // Delete Book by ID
+    @Operation(
+            summary = "Delete a Book by ID",
+            description = "To delete an book detail from MongoDB by their unique ID"
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBook(@PathVariable String id) {
         if (bookService.bookExists(id)) {
@@ -55,6 +86,10 @@ public class BookController {
     }
 
     // Update Book by ID
+    @Operation(
+            summary = "Update a Book by ID",
+            description = "To Update an book detail in MongoDB by their unique ID"
+    )
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable String id, @RequestBody Book book) {
         if (bookService.bookExists(id)) {
--- src/main/java/com/mongodb/restApi/services/BookService.java
+++ src/main/java/com/mongodb/restApi/services/BookService.java
@@ -1,10 +1,14 @@
 package com.mongodb.restApi.services;
 
 
+import com.mongodb.restApi.config.WebClientConfig;
 import com.mongodb.restApi.entity.Book;
 import com.mongodb.restApi.repository.BookRepository;
+import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +18,31 @@ public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    // Fetch Data From Rest API
+    public String insertBookData(String id){
+        String apiUrl = "https://softwium.com/api/books/" + id;
+        Book bookData =   webClientBuilder.build()
+                .get()
+                .uri(apiUrl)
+                .retrieve()
+                .bodyToMono(Book.class) // Convert response to Book object
+                .block();  //Wait for Completion of Call.
+        if(bookData==null) {
+            return "Failed to fetch data from the Book API.";
+        }
+        Optional<Book> existingData = bookRepository.findById(id);
+        if(existingData.isEmpty()){
+            bookRepository.save(bookData);
+            return "Data inserted successfully.";
+        }
+        else {
+            return "Data already exists in Database.";
+        }
+    }
 
     // Create or Update Book
     public Book saveBook(Book book) {
@@ -39,4 +68,6 @@ public class BookService {
     public boolean bookExists(String id) {
         return bookRepository.existsById(id);
     }
+
+
 }
